ef1ed38229cda9de8a9c30d7c5c9ce6ebdd6cdf5
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-03-04 23:54:52 +0530

    Enhance 04_objects_part_2.js with deep and shallow copy examples, and clarify merging objects with updated comments

--- 02_basics/04_objects_part_2.js
+++ 02_basics/04_objects_part_2.js
@@ -11,14 +11,32 @@ hero.realName = {firstName: 'Wilson', lastName: 'Fisk'}; // We can give object i
 console.log(hero); // See the output.
 */
 
-let obj1 = {
+const obj1 = {
     1: 'a',
     2: 'b'
 };
-let obj2 = {
+const obj2 = {
     3: 'c',
     4: 'd'
 };
+
+// --- --- DEEP AND SHALLOW COPY --- ---
+/*
+const shallowObj = obj1;
+// This will create a shallow copy (means, it uses reference).
+// To create a actually or deep copy we use a structuredClone() method.
+const deepObj = structuredClone(obj2);
+// Now the main difference is that deep copy creates a new copy of the object, which on change doesn't affect the original object.
+// And on the other hand a shallow copy is just refer to the original object and if we make any change
+// in this copy it will also change the original object.
+shallowObj[1] = "Apple"
+deepObj[3] = "Apple"
+console.log('shallowObj =', shallowObj, '\nobj1 =', obj1) 
+console.log('deepObj =', deepObj, '\nobj2 =', obj2)
+// You can see the in the output the obj1 got changed but not obj2
+*/
+
+
 
 // --- --- MERGING OBJECTS --- ---
 /*
@@ -37,4 +55,8 @@ console.log(mergedObject);
 const obj3 = {...obj1, ...obj2};
 console.log(obj3);
 // This spread operator also do the same thing, we have talked about it in arrays.
-// And we don't use assign() method in most cases, spread syntax is used mostly.
+// And we don't use assign() method in most cases, spread syntax is mostly preferred.
+
+// NOTE: Both assign() and spread operator they did create a deep copy but not for nested elements,
+// like if we have object inside a object then they create a deep copy of the main object but refers 
+// the object stored in it as a element, and that's why if we need a actually copy we use structuredClone().
