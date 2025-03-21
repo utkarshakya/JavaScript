fb4e3cca13ede36081856e316cf72b852317e3fc
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-03-21 20:52:03 +0530

    Improve comments in 10_filter.js for clarity on filter method and its comparison with forEach

--- 04_control_flow/10_filter.js
+++ 04_control_flow/10_filter.js
@@ -1,12 +1,12 @@
 // --- --- filter() Method --- ---
 const num = [1, 3, 4, 5, 6];
 // console.log(num.forEach((e) => {return e+3;}));
-// If you try to run thr above line of code you'll get undefined, that means we can not return
+// If you try to run the above line of code you'll get undefined, that means we can not return
 // any value from callback function in for each loop. And to cater this problem we use filter.
 let result = num.filter(e => e > 3);
 console.log(result);
 // Although we can get values from forEach callback function using some
-// conditional statements, but filter works in simply.
+// conditional statements, but filter works easily.
 // Like see the below example from that we also get the same result.
 let arr = [];
 num.forEach(e => {
@@ -29,5 +29,8 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 let myRecords = books.filter((e) => e.genre === "Fiction");
+// let myRecords = books.filter(({genre}) => genre === "Fiction"); // Here we destructure the object, it also work the fine.
 console.log(myRecords);
 // So you see in the output, filter make things so easy.
+
+// Filter also takes the same three arguments like arr.filter((num, index, arr) => return_statement)
